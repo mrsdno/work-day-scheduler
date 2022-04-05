8488f9f10b3133556d0f903d5ab1735857e9d81d
mrsdno <christine@dno.dev>
2022-04-05 16:41:31 -0400

    save updated task text to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,50 @@
 var schedule = {
+    eightAM: [],
+    nineAM: [],
 };
 
-// time block clicked to enter task
+// time block paragraph clicked to enter task
 $(".time-block").on("click", "p", function() {
-    console.log("you clicked content!");
+    var task = $(this)
+    .text()
+    .trim();
+
+    //create new input element
+    var taskInput = $("<input>")
+    .attr("type", "text")
+    .val(task);
+
+
+    $(this).replaceWith(taskInput);
+
+    //auto focus
+    taskInput.trigger("focus");
 })
+
+//click off time block paragraph to save new task
+
+$(".time-block").on("blur", "input[type='text']", function(){
+    //get current text
+    var task = $(this)
+    .val()
+    .trim();
+
+    // get the parent tasks ID
+    var taskTime = $(this)
+    .closest(".task")
+    .attr("id");   
+
+    // update task array and save to local storage
+
+    schedule[taskTime].text = task;
+
+    console.log(schedule)
+
+    saveTask();
+    
+
+})
+
+var saveTask = function(){
+    localStorage.setItem("schedule", JSON.stringify(schedule));
+}

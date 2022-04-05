cff905913f9d4829bfa4d0df84a7fbe502242cdc
mrsdno <christine@dno.dev>
2022-04-05 19:49:35 -0400

    updated CSS formatting for better look

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,9 @@ var schedule = {
     nineAM: [],
 };
 
+
 // time block paragraph clicked to enter task
-$(".time-block").on("click", "p", function() {
+$(".time-block").on("click", "span", function() {
     var task = $(this)
     .text()
     .trim();
@@ -12,6 +13,7 @@ $(".time-block").on("click", "p", function() {
     //create new input element
     var taskInput = $("<input>")
     .attr("type", "text")
+    .addClass("enter-task")
     .val(task);
 
 
@@ -21,30 +23,74 @@ $(".time-block").on("click", "p", function() {
     taskInput.trigger("focus");
 })
 
-//click off time block paragraph to save new task
+//click save button block paragraph to save new task
 
-$(".time-block").on("blur", "input[type='text']", function(){
-    //get current text
-    var task = $(this)
-    .val()
-    .trim();
+$(".saveBtn").on("click", function(){
 
     // get the parent tasks ID
     var taskTime = $(this)
-    .closest(".task")
-    .attr("id");   
+    .attr("id");  
+
+    //get current text
+    var task = $("input[type='text']")
+    .val()
+    .trim();
+
+    if (task) {
+    //create new div element to display new task
+    var updatedTask = $("<span>")
+    .addClass("enter-task")
+    .text(task);
+
+    // display new task after updating
+
+    $("input[type='text']").replaceWith(updatedTask);
+    }
+
+    else {
+        // assign text to empty string
+        var updatedTask = $("<span>")
+        .addClass("enter-task")
+        .addClass("col-8")
+        .text(" ");
+
+        // display new task after updating
+        $("input[type='text']").replaceWith(updatedTask);
+    }
 
     // update task array and save to local storage
+    schedule[taskTime] = task;
+    console.log(taskTime);
+    console.log(this);
+    console.log(schedule);
 
-    schedule[taskTime].text = task;
-
-    console.log(schedule)
 
     saveTask();
-    
-
 })
 
+
+
 var saveTask = function(){
+    
     localStorage.setItem("schedule", JSON.stringify(schedule));
+
+
 }
+
+var loadSchedule = function(schedule) {
+
+    schedule = JSON.parse(localStorage.schedule);
+
+    // load 8 am task 
+    $("#eight-am-task").remove(".holder-task")
+    $("#eight-am-task").append("<span>" + schedule.eightAM + "</span>")
+    .addClass("enter-task");
+
+    // load 9 am task 
+    $("#nine-am-task").remove(".holder-task")
+    $("#nine-am-task").append("<span>" + schedule.nineAM + "</span>")
+    .addClass("enter-task");
+
+}
+
+loadSchedule();

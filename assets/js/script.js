d03e8c831df7b7f7d42993fed33ce2db582f62bc
mrsdno <christine@dno.dev>
2022-04-06 22:29:11 -0400

    commented code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,30 @@
+// set up empty schedule variable for storage
 var schedule = {};
+// set now to current time to display in the header
 var now = moment().format('MM/DD/YYYY');
+
+function auditTimes() {
+    // set present to current hour in military time
+    var present = parseInt(moment().format("HH"));
+    // set up variable to assign hour to each .task-box, starting at 8 am
+    let time = 8;
+
+    // loop over each task box and compare present time to hour in .task-box
+    $(".task-box").each(function(){
+        if (present > time) {
+           $(this).addClass("future");
+        }
+
+        else if (present < time) {
+            $(this).addClass("past");
+        }
+
+        else {
+            $(this).addClass("present");
+        }
+        time ++;
+    })
+}
 
 // time block paragraph clicked to enter task
 $(".time-block").on("click", ".task", function() {
@@ -13,25 +38,25 @@ $(".time-block").on("click", ".task", function() {
     .addClass("enter-task")
     .val(task);
 
-
+    //replace .task with taskInput so text can be entered
     $(this).replaceWith(taskInput);
 
-    //auto focus
+    //auto focus on taskInput
     taskInput.trigger("focus");
+
 })
 
+
 //click save button block paragraph to save new task
-
 $(".saveBtn").on("click", function(){
-
-    // get data from local storage if it exists
+    
+    // get data from local storage if it exists and parse into a new array
     if (localStorage.schedule) {
         savedSchedule = localStorage.getItem("schedule");
-
         savedSchedule = JSON.parse(savedSchedule);
     }
     
-    // if its not, set up an empty string we can update
+    // if it does not exist, set up an empty string we can update
     else {
         savedSchedule = {
             eightAM: [],
@@ -56,18 +81,19 @@ $(".saveBtn").on("click", function(){
     var task = $("input[type='text']")
     .val();
 
+
     // if there is updated text
     if (task) {
-    //create new div element to display new task
-    var updatedTask = $("<span>")
-    .text(task)
-    .addClass("task");
+        //create new div element to display new task
+        var updatedTask = $("<span>")
+        .text(task)
+        .addClass("task");
 
-    // display new task after updating
-    $("input[type='text']").replaceWith(updatedTask)
-    
+        // display new task after updating
+        $("input[type='text']").replaceWith(updatedTask)
     }
-
+    
+    // if task was deleted, empty the input box
     else {
         // assign text to empty string
         var updatedTask = $("<span>")
@@ -79,11 +105,7 @@ $(".saveBtn").on("click", function(){
         $("input[type='text']").replaceWith(updatedTask);
     }
 
-    console.log(taskTime);
-    console.log(this);
-    console.log(savedSchedule);
     // update task array and save to local storage
-
     if (task) {
     savedSchedule[taskTime] = task;
     }
@@ -93,6 +115,9 @@ $(".saveBtn").on("click", function(){
     }
 
     saveTask(savedSchedule);
+
+    // update colors when you save the task
+    auditTimes();
 })
 
 
@@ -101,9 +126,10 @@ var saveTask = function(savedSchedule){
     localStorage.setItem("schedule", JSON.stringify(savedSchedule));
 }
 
+
 var loadSchedule = function(schedule) {
 
-    // add date to the header
+    // add date to the header on page load
     $("#currentDay").append(now);
 
     if (localStorage.schedule) {
@@ -166,22 +192,6 @@ var loadSchedule = function(schedule) {
         };
     }
 }
-   
-var auditTimes = function() {
-
-    const times = document.querySelectorAll('.task');
-    const count = times.length;
-    let time = 8;
-
-
-    for (let i=0; i < count; i++){
-        var currentTime = times[i].id;
-        console.log(currentTime, time);
-        time ++;
-    }
-
-}
-
 
 loadSchedule();
 auditTimes();
